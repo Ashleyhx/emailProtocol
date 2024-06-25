f8a20b0a486334d5ac1f978704a3cf864e5af6c3
Ashley Huo <x2huo@uwaterloo.ca>
2024-06-25 18:19:06 -0400

    seperate pop3 and imap

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import os
-
 from flask import Flask, render_template
-import imaplib
-import email
-from email.header import decode_header
+from email_client import fetch_emails_imap, fetch_emails_pop3
 
 app = Flask(__name__)
 
@@ -16,62 +13,18 @@ def index():
     return render_template("index.html")
 
 
-def fetch_emails():
-    imap_url = 'imap.gmail.com'
-
-    mail = imaplib.IMAP4_SSL(imap_url)
-    mail.login(username, password)
-    mail.select('inbox')
-
-    # Get total number of emails
-    res, messages = mail.select('inbox')
-    messages = int(messages[0])
-
-    # Specify the number of emails to fetch
-    n = 3
-    email_data = []
-
-    # Fetch n most recent emails
-    for i in range(messages, messages - n, -1):
-        res, msg = mail.fetch(str(i), '(RFC822)')
-        for response in msg:
-            if isinstance(response, tuple):
-                # Parse the bytes email into a message object
-                msg = email.message_from_bytes(response[1])
-
-                # Decode email sender
-                From = decode_header(msg['From'])[0][0]
-                if isinstance(From, bytes):
-                    From = From.decode()
-
-                # Decode email subject
-                subject = decode_header(msg['Subject'])[0][0]
-                if isinstance(subject, bytes):
-                    subject = subject.decode()
-
-                # Assume there might be multiple parts
-                body = ''
-                if msg.is_multipart():
-                    for part in msg.walk():
-                        content_type = part.get_content_type()
-                        content_disposition = str(part.get("Content-Disposition"))
-                        if content_type == 'text/plain' and 'attachment' not in content_disposition:
-                            body = part.get_payload(decode=True).decode()  # decode
-                            break
-                else:  # extract text/plain emails
-                    body = msg.get_payload(decode=True).decode()
-
-                email_data.append({'from': From, 'subject': subject, 'body': body})
-
-    mail.logout()
-    return email_data
+@app.route('/emails/imap')
+def show_emails_imap():
+    emails = fetch_emails_imap(username, password)
+    return render_template('emails.html', emails=emails)
 
 
-@app.route('/emails')
-def show_emails():
-    emails = fetch_emails()
+@app.route('/emails/pop3')
+def show_emails_pop3():
+    emails = fetch_emails_pop3(username, password)
     return render_template('emails.html', emails=emails)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
---
+++ email_client.py
@@ -0,0 +1,71 @@
+import imaplib
+import poplib
+import email
+from email.header import decode_header
+
+
+def fetch_emails_imap(username, password):
+    imap_url = 'imap.gmail.com'
+    mail = imaplib.IMAP4_SSL(imap_url)
+    mail.login(username, password)
+    mail.select('inbox')
+
+    res, messages = mail.select('inbox')
+    messages = int(messages[0])
+    n = 3
+    email_data = []
+
+    for i in range(messages, messages - n, -1):
+        res, msg = mail.fetch(str(i), '(RFC822)')
+        email_data.extend(parse_email(msg))
+
+    mail.logout()
+    return email_data
+
+
+def fetch_emails_pop3(username, password):
+    pop3_url = 'pop.gmail.com'
+    mail = poplib.POP3_SSL(pop3_url)
+    mail.user(username)
+    mail.pass_(password)
+
+    num_messages = len(mail.list()[1])
+    n = min(num_messages, 3)
+    email_data = []
+
+    for i in range(num_messages, num_messages - n, -1):
+        msg = mail.retr(i)[1]
+        msg = b"\n".join(msg).decode()
+        msg = email.message_from_string(msg)
+        email_data.append(parse_single_email(msg))
+
+    mail.quit()
+    return email_data
+
+
+def parse_email(messages):
+    email_data = []
+    for response in messages:
+        if isinstance(response, tuple):
+            msg = email.message_from_bytes(response[1])
+            email_data.append(parse_single_email(msg))
+    return email_data
+
+
+def parse_single_email(msg):
+    From = decode_header(msg['From'])[0][0]
+    From = From.decode() if isinstance(From, bytes) else From
+
+    subject = decode_header(msg['Subject'])[0][0]
+    subject = subject.decode() if isinstance(subject, bytes) else subject
+
+    body = ''
+    if msg.is_multipart():
+        for part in msg.walk():
+            if part.get_content_type() == 'text/plain':
+                body = part.get_payload(decode=True).decode()
+                break
+    else:
+        body = msg.get_payload(decode=True).decode()
+
+    return {'from': From, 'subject': subject, 'body': body}
